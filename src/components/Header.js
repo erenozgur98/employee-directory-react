5fc53e89b5a427b1518bec32aae9e508b979a9a3
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-09 20:33:19 -0400

    added more content to the app

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 function Header() {
     return (
-        <div className="">
-            <header className="">
-                <h1 className=""> Employee Directory </h1>
-            </header>
+        <div className="header">
+            <h1 className=""> Employee Directory </h1>
         </div>
     )
 };
