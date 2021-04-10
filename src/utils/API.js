e5d338922f2bc1f330ccd8e48c404180e5fb643a
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-09 20:52:09 -0400

    changed the content inside API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ const BASEURL = 'https://randomuser.me/api/?results=200&nat=us';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    getUsers: function(query) {
-        return axios.get(BASEURL + query);
+    getUsers: function() {
+        return axios.get(BASEURL);
     }
 };
