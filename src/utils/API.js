43a2fd4e4b11706935aa24530ee009b19c6b1305
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-12 23:55:35 -0400

    fixed a bug

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 const BASEURL = 'https://randomuser.me/api/?results=25&nat=us';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     getUsers: function() {
         return axios.get(BASEURL);
